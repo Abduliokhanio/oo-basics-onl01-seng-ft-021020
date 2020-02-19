0d540b28b5198bac69c6cd503517ba720bf16d1c
Abdul Khan <akhan17@outlook.com>
2020-02-19 18:04:22 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,47 @@
-
+class Book
+  #getters
+  #=======================================================
+  def initialize(title)
+    @title = title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+   #=======================================================
+  #setters
+  def title
+    @title
+  end 
+  
+  def author
+    @author
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre
+    @genre
+  end
+  
+  
+  #==========================================================
+  #methods
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+  
+end
 
