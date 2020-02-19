2f1d28311bf2fd3fbc07761732f7be1aadf55848
Abdul Khan <akhan17@outlook.com>
2020-02-19 18:41:32 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
@@ -11,3 +12,29 @@ class Book
   end
  
 end
+=======
+class Book
+  #getters
+  def initialize(title)
+    @title = title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def page
+  
+  
+  @setters
+  def title
+    @title
+  end 
+  
+  def author
+    @author
+  end
+  
+end
+
+>>>>>>> c2e96921bc9158ebed29c0d5b8af8b8ec92a2615
